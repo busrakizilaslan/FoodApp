f00f7aaa2215b5f91cd1fe4612d7548c01d6f3d4
Büşra KIZILASLAN YALÇIN <busra_kizilaslan@hotmail.com>
2022-07-31 19:36:57 +0300

    add homepage toolbar and option menu

--- app/src/main/java/com/busrayalcin/foodapp/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/busrayalcin/foodapp/ui/fragment/HomeFragment.kt
@@ -1,18 +1,30 @@
 package com.busrayalcin.foodapp.ui.fragment
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.busrayalcin.foodapp.R
 import com.busrayalcin.foodapp.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment() {
+@AndroidEntryPoint
+class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
     private lateinit var binding : FragmentHomeBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+        setHasOptionsMenu(true)
+
+
     }
 
     override fun onCreateView(
@@ -20,9 +32,44 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+//        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+
+        binding.toolbarHomeTitle = ""
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbarHome)
+//        binding.toolbarHome.setNavigationIcon(R.drawable.logofoodies)
+
+        //Show logo instead of toolbar title
+        binding.toolbarHome.setLogo(R.drawable.logofoodies)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu,menu)
+
+        val item = menu.findItem(R.id.action_search)
+        val searchView = item.actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_signout){
+            auth.signOut()
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToLoginFragment())
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        println("onQueryTextSubmit")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        println("onQueryTextChange")
+        return true
     }
 
 }
