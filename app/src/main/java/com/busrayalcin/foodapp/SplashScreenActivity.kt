7242efba1cc194857702fc177e8a6d283cece8f4
Büşra KIZILASLAN YALÇIN <busra_kizilaslan@hotmail.com>
2022-08-08 00:37:25 +0300

    update splash screen time

--- app/src/main/java/com/busrayalcin/foodapp/SplashScreenActivity.kt
+++ app/src/main/java/com/busrayalcin/foodapp/SplashScreenActivity.kt
@@ -15,8 +15,7 @@ class SplashScreenActivity : AppCompatActivity() {
         val background = object : Thread(){
             override fun run() {
                 try {
-
-                    Thread.sleep(1000)
+                    Thread.sleep(2000)
                     val intent = Intent(baseContext, MainActivity::class.java)
                     startActivity(intent)
                     finish()
